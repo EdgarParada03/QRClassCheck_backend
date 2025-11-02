7d763c64bf6fecd96620841e930e40bb6d40c2cf
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-11-01 21:56:18 -0500

    Agrego ruta de reportes

--- index.js
+++ index.js
@@ -5,18 +5,18 @@ const bodyParser = require('body-parser');
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
+app.use(express.json()); // ya no lo repetimos más abajo
 
-// Rutas
+// Rutas principales
 app.use('/usuarios', require('./routes/usuarios'));
 app.use('/asignaturas', require('./routes/asignaturas'));
 app.use('/semestres', require('./routes/semestres'));
 app.use('/clases', require('./routes/clases'));
 app.use('/asistencia', require('./routes/asistencia'));
+app.use('/auth', require('./routes/auth'));
 
-//Validacion o autenticación cuentas google
-const authRoutes = require('./routes/auth');
-app.use(express.json());
-app.use('/auth', authRoutes);
+// 🚀 Nueva ruta para reportes
+app.use('/reporte', require('./routes/reporte'));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
