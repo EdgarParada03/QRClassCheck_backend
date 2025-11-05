352ed7ccf31d19c8dec738116766a7bcdc926bc8
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-11-04 19:12:42 -0500

    Corrijo emailSender.js

--- utils/emailSender.js
+++ utils/emailSender.js
@@ -2,12 +2,18 @@ const nodemailer = require('nodemailer');
 
 const enviarCorreoConAdjunto = async (destinatario, pdfBuffer, nombreArchivo) => {
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true, // true para 465, false para 587
     auth: {
       user: process.env.CORREO_EMISOR,
       pass: process.env.CORREO_CLAVE
+    },
+    tls: {
+      rejectUnauthorized: false
     }
   });
+
 
   const mailOptions = {
     from: `"QRClassCheck" <${process.env.CORREO_EMISOR}>`,
